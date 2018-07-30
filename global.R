675fca47516b58a92e3adfe97d067b366702f7ec
Aaron Ferrucci <aaron.ferrucci@gmail.com>
2018-07-29 20:36:23 -0700

    Use w2w_utils to get cleaned data.

--- global.R
+++ global.R
@@ -1,7 +1,13 @@
 year <- 2018
-path_to_data <- "../../classes/data_science/various/wharf2wharf/w2w"
-datafile <- sprintf("%s%04d.csv", path_to_data, year)
-dataset <- read.csv(datafile)
+path_to_utils <- "../../classes/data_science/various/wharf2wharf"
+datafile <- sprintf("%s/w2w%d.csv", path_to_utils, year)
+utils <- sprintf("%s/w2w_utils.R", path_to_utils)
+source(utils)
+# Override utils' default path to data
+path_to_data <- path_to_utils
+
+dataset <- getCleanData(2018)
+
 # is used as "more than %d,000"
 thousands_of_participants = round(nrow(dataset) / 1000, 0)
 
